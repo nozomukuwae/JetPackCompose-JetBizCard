6ec8f37e887e0b34c8b7b2322a0aced5ada7aac3
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-08-28 16:37:04 +0900

    Add profile image

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -17,6 +21,8 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
@@ -46,7 +52,20 @@ fun CreateBizCard() {
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = 4.dp
         ) {
-//
+            Surface(
+                modifier = Modifier.size(150.dp).padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(width = 0.5.dp, color = Color.LightGray),
+                elevation = 4.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_image),
+                    contentDescription = "profile image",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
