c0d8312985ba8047b722aa0eeb6cd20903c09471
Nozomu Kuwae <nozomukuwae@gmail.com>
2023-08-28 19:13:03 +0900

    Populate portfolio

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -32,8 +34,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
@@ -98,9 +102,9 @@ fun CreateBizCard() {
 }
 
 @Composable
-private fun CreateProfileImage() {
+private fun CreateProfileImage(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -111,7 +115,7 @@ private fun CreateProfileImage() {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "profile image",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
     }
@@ -161,11 +165,34 @@ private fun Portfolio() {
 private fun PortfolioContent(data: List<String>) {
     LazyColumn {
         items(data) { item: String ->
-            Text(
-                text = item,
-                color = MaterialTheme.colors.primaryVariant,
-                style = MaterialTheme.typography.h4
-            )
+            Card(
+                modifier = Modifier.fillMaxSize().padding(13.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                        .padding(7.dp)
+                ) {
+                    CreateProfileImage(modifier = Modifier.size(100.dp))
+                    Column(
+                        modifier = Modifier
+                            .padding(7.dp)
+                            .align(alignment = Alignment.CenterVertically)
+                    ) {
+                        Text(
+                            text = item,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "a great project",
+                            style = MaterialTheme.typography.body2
+                        )
+                    }
+                }
+            }
         }
     }
 }
